6da958fe24eb15c2956435bdaa3dfd3bfa748a3d
[Facundo <BocchettoNicolas@hotmail.com>
2024-06-10 16:10:25 -0300

    Actualizacion de header, menu responsive, colores y creando sector (Articulos Destacados)'

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+/* 
+document.addEventListener('scroll', function() {
+  const section = document.getElementById('header');
+  if (window.scrollY > 100) { // Puedes ajustar este valor según tus necesidades
+      section.style.opacity = '0';
+      section.style.visibility = 'hidden';
+  } else {
+      section.style.opacity = '3';
+      section.style.visibility = 'visible';
+  }
+});*/
